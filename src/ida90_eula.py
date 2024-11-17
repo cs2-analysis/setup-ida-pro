616d1a436389fa6b351904dd714b6bb28681ebb6
dumbasPL <29180158+dumbasPL@users.noreply.github.com>
2024-11-17 18:13:43 +0100

    fix EULA

--- src/ida90_eula.py
+++ src/ida90_eula.py
@@ -3,5 +3,5 @@ import os
 import idapro
 import ida_registry
 
-## Agree to EULA using this simple hack
-ida_registry.reg_write_int(f'EULA 90 {os.environ.get("USER")}', 1, '')
+## Agree to EULA
+ida_registry.reg_write_int('EULA 90', 1, '')
